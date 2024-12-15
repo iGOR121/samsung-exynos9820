b0cf6f6a995df7294d9ea4e4873b9b1e6292275c
THEBOSS <ehabhani59@gmail.com>
2024-12-15 12:10:03 +0300

    sync: mali: Remove naming mechanism on fences
    It lowers CPU time to be spent on the strlcpy and prevent 32 bytes of wastage for those naming fences... it is proven on several files that those naming fences are just there for debugging purposes only, yet.. it might cause suboptimal operations for others just because naming fences mechanism which at the end... it is only made for developmental purposes... not for users.
    
    Inspired and Adapted from https://github.com/kerneltoast/android_kernel_google_wahoo/commit/24627e6b0d7042f0c28b79a85f43f8d2f2f816d3
    
    As the original commit said... [Eliminate unnecessary operations on fence names.]

--- drivers/gpu/arm/bv_r32p1/mali_kbase_sync_android.c
+++ drivers/gpu/arm/bv_r32p1/mali_kbase_sync_android.c
@@ -473,7 +473,6 @@ int kbase_sync_fence_in_info_get(struct kbase_jd_atom *katom,
 
 	info->fence = katom->fence;
 	info->status = kbase_fence_get_status(katom->fence);
-	strlcpy(info->name, katom->fence->name, sizeof(info->name));
 
 	return 0;
 }
@@ -486,7 +485,6 @@ int kbase_sync_fence_out_info_get(struct kbase_jd_atom *katom,
 
 	info->fence = katom->fence;
 	info->status = kbase_fence_get_status(katom->fence);
-	strlcpy(info->name, katom->fence->name, sizeof(info->name));
 
 	return 0;
 }
